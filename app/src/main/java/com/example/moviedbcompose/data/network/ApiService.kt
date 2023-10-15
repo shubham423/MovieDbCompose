80a071749f07ba01d169c91decf5a5982d11d60a
shubham423 <shubh991172@gmail.com>
2023-10-15 18:43:47 +0530

    add paging source

--- app/src/main/java/com/example/moviedbcompose/data/network/ApiService.kt
+++ app/src/main/java/com/example/moviedbcompose/data/network/ApiService.kt
@@ -2,9 +2,12 @@ package com.example.moviedbcompose.data.network
 
 import com.example.moviedbcompose.data.model.MovieResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("movie/popular")
-    suspend fun getPopularMovies(): MovieResponse
+    suspend fun getPopularMovies(
+        @Query("page") page: Int = 0
+    ): MovieResponse
 }
--- app/src/main/java/com/example/moviedbcompose/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/moviedbcompose/data/repository/MovieRepositoryImpl.kt
@@ -1,16 +1,22 @@
 package com.example.moviedbcompose.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.example.moviedbcompose.data.model.Movie
 import com.example.moviedbcompose.data.network.ApiService
+import com.example.moviedbcompose.data.paging.PopularMoviesPagingSource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
-class MovieRepositoryImpl @Inject constructor(val apiService: ApiService) : MovieRepository {
+class MovieRepositoryImpl @Inject constructor(private val apiService: ApiService) : MovieRepository {
     override fun getPopularMovies(): Flow<PagingData<Movie>> {
-      return flow {
-
-      }
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = 20),
+            pagingSourceFactory = {
+                PopularMoviesPagingSource(api = apiService)
+            }
+        ).flow
     }
 }
