d199ebe69adc086b30a2696e7b61b85b71232a58
shubham423 <shubh991172@gmail.com>
2023-10-16 09:28:37 +0530

    fix ui for movie item

--- app/src/main/java/com/example/moviedbcompose/presentation/components/MovieItem.kt
+++ app/src/main/java/com/example/moviedbcompose/presentation/components/MovieItem.kt
@@ -2,10 +2,15 @@ package com.example.moviedbcompose.presentation.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.moviedbcompose.R
 import com.example.moviedbcompose.data.model.Movie
@@ -13,16 +18,29 @@ import com.example.moviedbcompose.utils.Constants.BASE_POSTER_IMAGE_URL
 
 @Composable
 fun MovieItem(movie: Movie) {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        val imagePath = "$BASE_POSTER_IMAGE_URL/${movie.posterPath}"
-        AsyncImage(
-            model = imagePath,
-            placeholder = painterResource(id = R.drawable.ic_launcher_background),
-            error = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null,
-        )
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Card {
+            val imagePath = "$BASE_POSTER_IMAGE_URL/${movie.posterPath}"
+            AsyncImage(
+                model = imagePath,
+                placeholder = painterResource(id = R.drawable.ic_launcher_background),
+                error = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+            )
+        }
         Text(
-            text = movie.title,
+            text = trimTitle(movie.title),
+            color = Color.White
         )
     }
+}
+
+fun trimTitle(text: String) = if (text.length <= 26) text else {
+    val textWithEllipsis = text.removeRange(startIndex = 26, endIndex = text.length)
+    "$textWithEllipsis..."
 }
